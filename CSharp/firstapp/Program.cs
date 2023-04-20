fc43f7a047b1fe178c1ed43867e74a326768a995
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-04-20 10:04:59 +0530

    Initial Commit

---
+++ CSharp/firstapp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hi, This is my first program in C#");
+Console.WriteLine(2);
+Console.Write("This is Line 1");
+Console.Write("This is Line 2");
+Console.WriteLine("Hello "+"World");
+
