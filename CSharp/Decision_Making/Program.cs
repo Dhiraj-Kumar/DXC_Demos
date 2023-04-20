7b0d0600cb92b38df8bb0a1ad3bbe4028143c65f
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-04-20 17:21:22 +0530

    updated

--- CSharp/Decision_Making/Program.cs
+++ CSharp/Decision_Making/Program.cs
@@ -1,14 +1,68 @@
-﻿//Logical Operatos - &&, ||, !
-//Age>=18 and qualification>=10
-Console.WriteLine("Enter Your Age");
-int age = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Enter Your Qualification");
-int qual = Convert.ToInt16(Console.ReadLine());
-if(age>=18 && qual >= 10)
-{
-    Console.WriteLine("You are eligible");
-}
-else
-{
-    Console.WriteLine("You are not eligible");
-}
+﻿//int day = 3;
+//switch (day)
+//{
+//    case 1:
+//        {
+//            Console.WriteLine("Monday");
+//            break;
+//        }
+//    case 2:
+//        {
+//            Console.WriteLine("Tuesday");
+//            break;
+//        }
+//    case 3:
+//        {
+//            Console.WriteLine("Wednesday");
+//            break;
+//        }
+//    case 4:
+//        {
+//            Console.WriteLine("Thursday");
+//            break;
+//        }
+//    case 5:
+//        {
+//            Console.WriteLine("Friday");
+//            break;
+//        }
+//    case 6:
+//        {
+//            Console.WriteLine("Saturday");
+//            break;
+//        }
+//    case 7:
+//        {
+//            Console.WriteLine("Sunday");
+//            break;
+//        }
+//    default:
+//        {
+//            Console.WriteLine("Wrong Choice");
+//            break;
+//        }
+//}
+
+
+//if (day == 1)
+//{
+//    Console.WriteLine("Monday");
+//}
+//else if(day == 2)
+//{
+//    Console.WriteLine("Tuesday");
+//}
+//else if (day == 3)
+//{
+//    Console.WriteLine("Wednesday");
+//}
+
+// Enter First Number 10
+// Enter Second Number 5
+// Enter the Operator +
+// Result is 15
+
+int a = 10, b = 20;
+//int c = a + b;
+//Console.WriteLine("The sum of " + a + " and " + b + " is " + c);
+Console.WriteLine($"The sum {a} and {b} is {a + b}");
