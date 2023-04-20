49756a6d93f124132185b49620787c561bc234ee
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-04-20 11:57:16 +0530

    updated

--- CSharp/Decision_Making/Program.cs
+++ CSharp/Decision_Making/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Logical Operatos - &&, ||, !
+//Age>=18 and qualification>=10
+Console.WriteLine("Enter Your Age");
+int age = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Your Qualification");
+int qual = Convert.ToInt16(Console.ReadLine());
+if(age>=18 && qual >= 10)
+{
+    Console.WriteLine("You are eligible");
+}
+else
+{
+    Console.WriteLine("You are not eligible");
+}
