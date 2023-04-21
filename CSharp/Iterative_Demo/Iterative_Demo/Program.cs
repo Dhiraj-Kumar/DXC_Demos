d56876bb94cc81e0dd63923331e2f03c3f3edcbe
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-04-21 10:18:20 +0530

    updated

---
+++ CSharp/Iterative_Demo/Iterative_Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿// Type of Loops
+// For, While, Do...While, For each
+
+//for(int i = 10; i >= 1; i--)
+//{
+//    Console.WriteLine(i);
+//}
+
+//int i = 1;
+//while (i <= 10)
+//{
+//    i++;
+//    Console.WriteLine(i);
+//}
+
+int i = 11;
+do
+{
+    Console.WriteLine(i);
+    i++;
+} while (i <= 10);
