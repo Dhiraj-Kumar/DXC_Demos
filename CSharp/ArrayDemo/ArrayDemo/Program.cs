242d3ef713528efdeb6acb94dd35da7caa766f5f
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-04-24 14:45:48 +0530

    updated

---
+++ CSharp/ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿int[] arr = new int[5];
+arr[0] = 10;
+
+
+int[] arr2 = { 10, 20, 30 };
+
+int[] arr3 = new int[] { 10, 20, 30, 40 };
+
+//Multi Dimensional Array
+int[,] num = new int[2, 3];
+
+num[0, 0] = 10;
+num[0, 1] = 20;
+
+int[,] num2 = new int[,] { { 1, 2, 3 }, { 5, 6, 7 } };
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write($"{num2[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int[,] num3 = { { 10, 20 }, { 30, 40 } };
+
+// Jagged Array
+int[][] jagArr = new int[3][];
+jagArr[0] = new int[] { 10, 20, 30 };
+jagArr[1] = new int[] { 1, 2 };
+jagArr[2] = new int[] { 5, 6, 7, 8 };
+
+for (int i = 0; i < jagArr.Length; i++)
+{
+    for (int j = 0; j < jagArr[i].Length; j++)
+    {
+        Console.Write(jagArr[i][j] + " ");
+    }
+    Console.WriteLine();
+}
+
+//int[] arr1 = new int[5];
+
+//for (int i = 0; i < arr1.Length; i++)
+//{
+//    arr1[i] = Convert.ToInt16(Console.ReadLine());
+//}
+
+//foreach (int item in arr1)
+//{
+//    Console.WriteLine(item);
+//}
+
+//for(int j = 0; j < arr1.Length; j++)
+//{
+//    Console.WriteLine(arr1[j]);
+//}
---
+++ CSharp/StringDemo/StringDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿string s = String.Empty;
+
+string s1 = "";
+
+string path = @"C:\Folder1\File1.txt";
+
+string str = "Welcome to CSharp";
+string res = str.Remove(0, 3);
+Console.WriteLine(res);
+
+string s2 = str.Insert(11, "Microsoft ");
+Console.WriteLine(s2);
+
+Console.WriteLine(str.Substring(8, 2));
+
+string s3 = null;
+s3 = String.Empty;
+
+string name = "John";
+Console.WriteLine(name.IndexOf('o'));
+
+string message = "Welcome to My First Program";
+Console.WriteLine(message.IndexOf('o'));
+Console.WriteLine(message.LastIndexOf('o'));
+
+string str1 = "hello";
+string str2 = "Hello";
+
+Console.WriteLine(str1.CompareTo(str2));
+Console.WriteLine(String.Compare(str1, str2));
+Console.WriteLine(str1.Equals(str2, StringComparison.OrdinalIgnoreCase));
+
+string msg = "Hello and welcome to the concept of string";
+Console.WriteLine(msg.StartsWith('H'));
+
+string[] result = msg.Split(" ");
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
