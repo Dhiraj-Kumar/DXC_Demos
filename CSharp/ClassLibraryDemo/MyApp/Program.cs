30cf3b2b468c6c57cfa9f442544a6f9bfcdf1ed5
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-04-26 17:35:07 +0530

    updated

---
+++ CSharp/ClassLibraryDemo/MyApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyLibrary;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine(calc.Sum(10,20));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CSharp/ClassLibraryDemo/MyLibrary/Calculator.cs
@@ -0,0 +1,20 @@
+﻿namespace MyLibrary
+{
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
