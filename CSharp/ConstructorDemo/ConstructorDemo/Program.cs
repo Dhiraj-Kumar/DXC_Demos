efaf6dd0f9d4baff4e2ba3a9525d3495aaad80f4
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-04-26 10:55:05 +0530

    updated

---
+++ CSharp/ConstructorDemo/ConstructorDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConstructorDemo
+{
+    class Demo
+    {
+        public Demo()
+        {
+            Console.WriteLine("I am constructor");
+        }
+        public Demo(int x)
+        {
+            Console.WriteLine("This is another constructor");
+        }
+        public void Show()
+        {
+            Console.WriteLine("This is show method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
+            Demo d2 = new Demo(10);
+            d.Show();
+        }
+    }
+}
---
+++ CSharp/StaticDemo/StaticDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace StaticDemo
+{
+    class Demo
+    {
+        public static int num = 0;
+        public void Increment()
+        {
+            num++;
+            Console.WriteLine(num);
+        }
+
+        public static void Show()
+        {
+            Console.WriteLine("This is Show Method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d1 = new Demo();
+            Demo d2 = new Demo();
+            Demo d3 = new Demo();
+            d1.Increment();
+            d2.Increment();
+            d3.Increment();
+
+            Demo.Show();
+            
+        }
+    }
+}
+
+// Type of Access Specifier
+// 1. public - Can access anywhere
+// 2. private - Can access only inside the class
+// 3. internal - Can access within the assembley
+// 4. protected - Can access within the class or its derived classes
+// 5. protected internal - Comprise the features of protected as well as internal
