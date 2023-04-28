33b9d6209b8fc8cf581cf39433279e15881b9d3c
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-04-28 14:58:54 +0530

    updated

---
+++ CSharp/CollectionsDemo/CollectionsDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(4, "Four");
+            ht.Add(1, "One");
+            ht.Add(3, "Three");
+            ht.Add(2, "Two");
+            ht.Add(5, "Five");
+
+            foreach (var item in ht.Keys)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
