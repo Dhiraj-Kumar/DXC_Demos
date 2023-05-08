aa370b97a320e306a1c946fe3d9373f429c3f3b9
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-05-08 17:10:44 +0530

    updated

--- SQL/08-May-2023-Day1.sql
+++ SQL/08-May-2023-Day1.sql
@@ -80,7 +80,62 @@ Insert into Customers (CustomerId, CustomerName, Email, Age) values (3, 'Peter',
 Insert into Customers values (4, 'Steve', 'Steve@gmail.com', 'Chennai', 27),(5, 'Maria', 'Maria@gmail.com', 'Kolkata', 25)
 
 -- Modifying record using Update statement
+Update Customers set City='Pune' where CustomerId=3
+Update Customers set City='Hyderabad', Email='Maria@hotmail.com' where CustomerId=5
 
-
+-- Delete row from a table
+Delete from Customers where CustomerId=3
 
 Select * from Customers
+
+
+Use Northwind
+
+Select * from Customers
+
+Select CustomerId, ContactName, City, Country from Customers
+
+Select * from Customers where CustomerID='BERGS'
+
+Select * from Customers where City='London'
+
+Select CustomerId, ContactName, City, Country from Customers where ContactTitle='Owner'
+
+Select * from Products where UnitPrice>35
+
+Select * from Customers where City='London' or City='Madrid'
+
+Select * from Customers where Country='Brazil' and City='Sao Paulo'
+
+Select * from Customers where Region is not null
+
+Select * from Customers where not Region is null
+
+Select * from Customers where not Country='Mexico'
+
+Select ProductID, ProductName, QuantityPerUnit, UnitPrice, UnitsInStock, UnitPrice*UnitsInStock as Amount, (UnitPrice*UnitsInStock)*10/100
+as Discount from Products
+
+Select * from Products where ProductID not between 11 and 20
+
+Select * from Customers where City='London' or City='Madrid' or City='Paris' or City='Berlin'
+
+Select * from Customers where City in ('London', 'Madrid', 'Paris', 'Berlin')
+
+Select * from Customers where ContactName like 'A%'
+
+Select * from Customers where ContactName like 'A_____________'
+
+Select * from Customers where ContactName like '[^ASJ]%'
+
+Select * from Customers where ContactName like 'A%S'
+
+Select top 10 * from Customers
+
+Select * from Customers Order By ContactName Desc
+
+Select * from Customers
+
+Select * from Customers Order By CustomerID offset 10 rows fetch next 5 rows only
+
+Select distinct ContactTitle from Customers
