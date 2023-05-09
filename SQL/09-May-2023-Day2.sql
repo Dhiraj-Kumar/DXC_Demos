3ab73f821da2493b35f79dbcb321990284fceadd
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-05-09 16:28:36 +0530

    updated

--- SQL/09-May-2023-Day2.sql
+++ SQL/09-May-2023-Day2.sql
@@ -85,6 +85,16 @@ Select * from Students order by Marks Desc offset 3 rows fetch next 1 rows only
 
 
 /*
+Problem Statement
+Note management app (similar to Google Keep) is used to take notes, add notes into categories and set reminders for a note. Create the necessary DB schema (SQL Server) including tables, relationships and add sample data into each table.
+
+Expected Solution
+Create the tables for Note, Category, Reminder, User, NoteReminder and NoteCategory.
+User table fields: user_id, user_name, user_added_date, user_password, user_mobile
+Note table fields: note_id, note_title, note_content, note_status, note_creation_date
+Category table fields : category_id, category_name, category_descr, category_creation_date, category_creator
+Reminder table fields : reminder_id, reminder_name, reminder_descr, reminder_type, reminder_creation_date, reminder_creator
+Insert the rows into the created tables (Note, Category, Reminder, User, NoteReminder and NoteCategory) as per attached snapshot.
 Write the queries to carry out the below listed operations:
 
 Fetch the row from User table with Id=112233 and Password=’Maya1214’.
@@ -97,5 +107,67 @@ Fetch all the reminder details for note id=2.
 Write a query to change the note_status to ‘Completed’ with note Id=3.
 Write a query to set a reminder of type ‘Personal Reminders’ for Note with note id=1. [Find reminder id based upon reminder_type]
 Write a query to remove all reminders from Note with note_id=2.
-
 */
+
+-- Creating Views
+Alter View ProductDetailsView
+as
+Select ProductId, ProductName, UnitPrice, UnitsInStock, UnitPrice*UnitsInStock as Amount, (UnitPrice*UnitsInStock)*10/100 as Discount from Products
+
+Select * from ProductDetailsView
+
+-- Creating Functions (UDF - User Defined Function)
+-- Scalar function returns single value
+Create Function Cube(@num int)
+returns int
+as
+begin
+	RETURN POWER(@num, 3)
+end
+
+Select dbo.Cube(5)
+
+-- Table Valued Function (Inline)
+Create Function GetCustomer(@city varchar(10))
+returns table
+as
+return (Select * from Customers where City=@city)
+
+Select * from GetCustomer('Paris')
+
+-- Stored Procedures
+
+Create Proc ShowCustomers
+as
+Select * from Customers
+
+Exec ShowCustomers
+
+Create Procedure SearchCustomer
+@customerid varchar(5)
+as
+Select * from Customers where CustomerID=@customerid
+
+Exec SearchCustomer 'BERGS'
+
+Select * from Students
+
+Alter Proc AddNewStudent
+@rollno int,
+@name varchar(10),
+@marks int
+as
+begin
+if(@marks>100)
+begin
+	print 'Marks cannot be greater than 100'
+	rollback tran
+end
+else
+	begin
+		insert into Students values(@rollno, @name, @marks)
+	end
+end
+
+Exec AddNewStudent 12, 'Jessabel', 98
+
