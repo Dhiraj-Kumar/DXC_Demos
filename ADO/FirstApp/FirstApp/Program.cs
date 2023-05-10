8a71796a73635392ad4620bb39f4585f3962c3a7
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-05-10 12:47:14 +0530

    updated

--- ADO/FirstApp/FirstApp/Program.cs
+++ ADO/FirstApp/FirstApp/Program.cs
@@ -6,16 +6,26 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            SqlConnection con = new SqlConnection("Server=DHIRAJ-PC\\SQLEXPRESS; database=Northwind; Integrated Security=true");
+            SqlConnection con = new SqlConnection("Server=DHIRAJ-PC\\SQLEXPRESS; database=TestDB; Integrated Security=true");
             con.Open();
-            SqlCommand cmd = new SqlCommand("Select * from Customers", con);
-            SqlDataReader dr = cmd.ExecuteReader();
-            //Console.WriteLine(dr.FieldCount);
-            while (dr.Read())
-            {
-                //Console.WriteLine($"{dr["CustomerId"]} | {dr["ContactName"]} | {dr["ContactTitle"]} | {dr["Address"]} | {dr["City"]}");
-                Console.WriteLine($"{dr[0]} | {dr[1]} | {dr[2]} | {dr[3]} | {dr[4]}");
-            }
+            SqlCommand cmd = new SqlCommand($"insert into Customers values(@id, @name, @email, @city, @age)", con);
+            Console.WriteLine("Enter CustomerId: ");
+            int id = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter Customer Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Customer Email: ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Customer City: ");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter Customer Age: ");
+            int age = Convert.ToInt16(Console.ReadLine());
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@name",name);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@city", city);
+            cmd.Parameters.AddWithValue("@age", age);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Record saved successfully");
             con.Close();
         }
     }
