61c3a3d3d423a266b650b826af0c23c35b906232
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-05-11 11:31:13 +0530

    updated

--- ADO/FirstApp/FirstApp/Program.cs
+++ ADO/FirstApp/FirstApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace FirstApp
 {
@@ -7,26 +8,14 @@ namespace FirstApp
         static void Main(string[] args)
         {
             SqlConnection con = new SqlConnection("Server=DHIRAJ-PC\\SQLEXPRESS; database=TestDB; Integrated Security=true");
-            con.Open();
-            SqlCommand cmd = new SqlCommand($"insert into Customers values(@id, @name, @email, @city, @age)", con);
-            Console.WriteLine("Enter CustomerId: ");
-            int id = Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("Enter Customer Name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter Customer Email: ");
-            string email = Console.ReadLine();
-            Console.WriteLine("Enter Customer City: ");
-            string city = Console.ReadLine();
-            Console.WriteLine("Enter Customer Age: ");
-            int age = Convert.ToInt16(Console.ReadLine());
-            cmd.Parameters.AddWithValue("@id", id);
-            cmd.Parameters.AddWithValue("@name",name);
-            cmd.Parameters.AddWithValue("@email", email);
-            cmd.Parameters.AddWithValue("@city", city);
-            cmd.Parameters.AddWithValue("@age", age);
-            cmd.ExecuteNonQuery();
-            Console.WriteLine("Record saved successfully");
-            con.Close();
+            SqlDataAdapter adp = new SqlDataAdapter("Select * from Products", con);
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
+            ds.Tables[0].Rows[1].Delete();
+
+            SqlCommandBuilder builder = new SqlCommandBuilder(adp);
+            adp.Update(ds);
+            Console.WriteLine("Database Updated");
         }
     }
 }
