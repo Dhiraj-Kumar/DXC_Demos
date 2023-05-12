6ec1bd27a23ddba9fc5613b4621d6118f1d93f3d
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-05-12 11:03:48 +0530

    updated

---
+++ ADO/SpectreApp/SpectreApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Spectre.Console;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace SpectreApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.MarkupLine($"Welcome to My App {Emoji.Known.SmilingFace}");
+            var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                    .Title("[green] Select your option [/]?")
+                    .AddChoices(new[] {
+                        "Add Transaction", "View Expenses", "View Income",
+                        "Check Balance"
+            }));
+            Console.WriteLine($"You selected {choice}");
+        }
+    }
+}
