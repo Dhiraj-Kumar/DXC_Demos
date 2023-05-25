c5aefd7cb682c0d56f8dea16d42bb7df295e7147
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-05-25 12:41:41 +0530

    updated

---
+++ Azure Files/QueueDemo/MessageReceiver/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+
+namespace MessageReceiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string con =
+                "DefaultEndpointsProtocol=https;AccountName=stackroutestorage;AccountKey=ZudZXtqhDwiE0rRgpmQCGy3xdHCe1kCwJNovablTyb6DNR3ZDXit0KbIJRL9jouVDot7B5Cf27cu+AStOmZdZw==;EndpointSuffix=core.windows.net";
+            CloudStorageAccount account = CloudStorageAccount.Parse(con);
+            CloudQueueClient client = account.CreateCloudQueueClient();
+            CloudQueue queue = client.GetQueueReference("myqueue");
+            queue.CreateIfNotExists();
+            Console.WriteLine("---- Message Receiver ----");
+            Console.WriteLine("Waiting for messages....");
+            while (true)
+            {
+                CloudQueueMessage message = queue.GetMessage();
+                if (message != null)
+                {
+                    Console.WriteLine(message.AsString);
+                    queue.DeleteMessage(message);
+                }
+            }
+        }
+    }
+}
---
+++ Azure Files/QueueDemo/MessageSender/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+
+namespace MessageSender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string con =
+                "DefaultEndpointsProtocol=https;AccountName=stackroutestorage;AccountKey=ZudZXtqhDwiE0rRgpmQCGy3xdHCe1kCwJNovablTyb6DNR3ZDXit0KbIJRL9jouVDot7B5Cf27cu+AStOmZdZw==;EndpointSuffix=core.windows.net";
+            CloudStorageAccount account = CloudStorageAccount.Parse(con);
+            CloudQueueClient client = account.CreateCloudQueueClient();
+            CloudQueue queue = client.GetQueueReference("myqueue");
+            queue.CreateIfNotExists();
+            Console.WriteLine("---- Message Sender ----");
+            while (true)
+            {
+                Console.WriteLine("Type Your Message");
+                string msg = Console.ReadLine();
+                CloudQueueMessage message = new CloudQueueMessage(msg);
+                queue.AddMessage(message);
+                Console.WriteLine("Message Sent!");
+            }
+        }
+    }
+}
