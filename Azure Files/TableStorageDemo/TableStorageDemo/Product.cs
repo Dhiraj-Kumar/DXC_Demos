782fca574701f46a677496fc2650f0fb230a8d9c
Dhiraj Kumar <dhiraj.kumar@stackroute.in>
2023-05-25 17:20:13 +0530

    updated

---
+++ Azure Files/TableStorageDemo/TableStorageDemo/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableStorageDemo
+{
+    public class Product : ITableEntity
+    {
+        public string PartitionKey { get; set; } = Guid.NewGuid().ToString();
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public string ETag { get; set; }
+
+        public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        {
+            
+        }
+
+        public IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+        {
+            IDictionary<string, EntityProperty> myProperties = TableEntity.Flatten(this, operationContext);
+            myProperties.Remove("PartitionKey");
+            myProperties.Remove("RowKey");
+            myProperties.Remove("Timestamp");
+            myProperties.Remove("ETag");
+            return myProperties;
+        }
+    }
+}
---
+++ Azure Files/TableStorageDemo/TableStorageDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Xml.Linq;
+
+namespace TableStorageDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string con =
+                "DefaultEndpointsProtocol=https;AccountName=stackroutestorage;AccountKey=wn8rN0RSrQm7YHPSrwr8g4F1n1ieV/bAs2FeW9p9DGR9Ag1J6R3kjlrkMR73bWQVtHqPMSb5XKZ5+ASt9YBZ9g==;EndpointSuffix=core.windows.net";
+            CloudStorageAccount account = CloudStorageAccount.Parse(con);
+            CloudTableClient client = account.CreateCloudTableClient();
+            CloudTable table = client.GetTableReference("Product");
+            table.CreateIfNotExistsAsync();
+            var ops = TableOperation.Insert(new Product()
+            {
+                Name = "Laptop",
+                Brand = "Dell",
+                Quantity = 5,
+                Price = 76000
+            });
+            
+            var res = table.ExecuteAsync(ops);
+            
+            var entity = res.Result;
+            Console.WriteLine(entity);
+
+        }
+    }
+}
